8979be308251e93ae26096b12f0cc36cb0d4778e
VSondur123 <varunsondur@gmail.com>
2026-07-17 11:41:35 +0530

    Connect custom domain varunsondur.com; base path back to /

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/varun-sondur/',
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
